c3de7f84a8e4dd8faea026ab70935c61d0f47671
Bobby Ross <Bobby1985uk@gmail.com>
2017-01-21 13:19:05 +0000

    initial commit

---
+++ client/src/app.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+window.onload = function(){
+  const appDiv = document.getElementById('app');
+  ReactDOM.render(
+    <h1>Hello World</h1>, appDiv
+    );
+};
